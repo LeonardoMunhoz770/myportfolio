113f12b4e6f271bd2689170c267c33997d145514
LeonardoMunhoz770 <munhozleonardo756@gmail.com>
2023-07-01 14:11:13 -0300

    changed new projects

--- src/pages/home/portifolio/index.js
+++ src/pages/home/portifolio/index.js
@@ -9,9 +9,8 @@ import {
 } from "./styles";
 import Projects from "../../../components/projects";
 import imageBegrato from "../../../assets/begreatoLp.webp";
-import imageOrganograma from "../../../assets/valorant.webp";
-import imageMunhozCompany from "../../../assets/munhozCompany.webp";
 import imageMunhozCompanyReact from "../../../assets/munhozCompanyReact.webp";
+import imageFormCredit from "../../../assets/formCard.webp";
 
 const Portifolio = () => {
   const arrayProjects = [
@@ -25,26 +24,17 @@ const Portifolio = () => {
     },
     {
       description:
-        "Um organograma é um tipo de diagrama que representa a estrutura hierárquica de uma organização ou empresa. Ele mostra as relações e a distribuição de autoridade, responsabilidade e comunicação entre os diferentes cargos e departamentos. Neste caso, eu escolhi um tema de algo que eu gosto que é o jogo Valorant, para que ele crie cards de acordo com as classes dos personagens do jogo. Foi utilizado React em sintonia com o curso da Alura.",
-      imageProject: imageOrganograma,
-      titleProject: "Organograma Valorant",
-      url: "https://organograma-valorant.vercel.app/",
-      alt: "pagina de um organograma com o tema do jogo valorant",
-    },
-    {
-      description:
-        "Este projeto foi feito com a intenção de aprender a integração com firebase e todos os métodos de autenticação, seja ela com o google ou senha, e também o reset de senha utilizando um link enviado para o e-mail do usuário. Ele possui tela de login, de registro e um modal para reset de senha caso esqueça. Foi desenvolvido utilizando somente JavaScript com a integração com o banco de dados do Firebase.",
-      imageProject: imageMunhozCompany,
-      titleProject: "MunhozCompany Firebase",
-      url: "https://munhoz-project.vercel.app/",
-      alt: "tela de login e senha com o tema MunhozCompany",
-    },
-    {
-      description:
         "Este projeto em desenvolvimento é um sistema completo de autenticação com o mesmo sistema do firebase, porém agora utilizando react, react-router, styled components e a api do Firebase, nele é implementado as rotas privadas e públicas, sistema de cadastro, autenticação, tudo isso utilizando o token gerado pelo firebase para cada usuário",
       imageProject: imageMunhozCompanyReact,
       titleProject: "Sistema de Autenticação",
       url: "https://munhoz-company.vercel.app/",
+    },
+    {
+      description:
+        "O projeto consiste em um formulário de cadastro de cartão de crédito desenvolvido em ReactJS, usando a biblioteca styled-components para estilização. O formulário possui campos com formatação adequada, como Número do cartão, Nome do titular, Validade e Código de segurança. Os campos são dinâmicos e obrigatórios, permitindo apenas números nos campos numéricos e apenas letras nos campos de texto. Há também um cartão ao lado do formulário que é atualizado conforme o usuário digita os dados, incluindo a identificação da bandeira do cartão com base no algoritmo de Luhn. As bandeiras de cartão validadas são Visa, Mastercard, American Express, Discover, Diners Club e Maestro.",
+      imageProject: imageFormCredit,
+      titleProject: "Registro de Cartão de Crédito",
+      url: "https://form-credit-card-ten.vercel.app/",
     },
   ];
 
